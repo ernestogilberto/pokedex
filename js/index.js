31a163973fe83d53632e27c0aed20521430733be
Ernesto Gilberto Valenzuela Gallardo <ernestogilberto@hotmail.com>
2021-10-27 21:00:07 -0500

    Actualización de funciones. Habla y botones de dirección.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,56 @@
-import {findPokemon} from "./pokedex.js";
+import {setPokemon, setImage} from "./pokedex.js";
 
 const $form = document.querySelector('#form')
+const $next = document.querySelector('#next-pokemon')
+const $prev = document.querySelector('#prev-pokemon')
+const $pokedex = document.querySelector('#pokedex')
+const $nextImg = document.querySelector('#next-image')
+const $prevImg = document.querySelector('#prev-image')
 
 $form.addEventListener('submit', showPokemon)
+$next.addEventListener('click', showNextPokemon)
+$prev.addEventListener('click', showPrevPokemon)
+$nextImg.addEventListener('click', showNextImage)
+$prevImg.addEventListener('click', showPrevImage)
 
+let currentPokemon = null
+let currentImage = null
 
 async function showPokemon(event) {
+    $pokedex.classList.add('is-open')
     event.preventDefault()
     const form = new FormData($form)
     const id = form.get('id')
 
-    await findPokemon(id)
+    currentPokemon = await setPokemon(id)
+    currentImage = 0
+}
+
+async function showNextPokemon() {
+    const id = currentPokemon && currentPokemon.id < 893 ? currentPokemon.id + 1 : 1
+    currentPokemon = await setPokemon(id)
+    $form.querySelector('input[name="id"]').value = id
+}
+
+async function showPrevPokemon() {
+    const id = currentPokemon && currentPokemon.id > 1 ? currentPokemon.id - 1 : 893
+    currentPokemon = await setPokemon(id)
+    $form.querySelector('input[name="id"]').value = id
+}
+
+async function showNextImage() {
+    if(currentPokemon){
+        const id = currentImage < currentPokemon.sprites.length-1 ? ++currentImage : 0
+        currentImage = id
+        setImage(currentPokemon.sprites[id])
+    }
+}
+
+
+async function showPrevImage() {
+    if(currentPokemon){
+        const id = currentImage > 0 ? --currentImage : currentPokemon.sprites.length-1
+        currentImage = id
+        setImage(currentPokemon.sprites[id])
+    }
 }
--- js/pokedex.js
+++ js/pokedex.js
@@ -2,16 +2,66 @@ import {getPokemon, getSpecies} from "./api.js";
 
 const $image = document.querySelector('#image')
 const $description = document.querySelector('#description')
+const $screen = document.querySelector('#screen')
+const $light = document.querySelector('#light')
+
+function speech(text) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'es'
+    speechSynthesis.speak(utterance);
+    $light.classList.add('is-animated')
+
+    utterance.addEventListener
+   ('end', () => {     $light.classList.remove('is-animated')
+    })
+}
+
+export function setImage(url) {
+    $image.src = url
+}
+
+function setDescription(name, description){
+    $description.innerHTML = `
+            <h1>${name}</h1>
+            <p>${description}</p>`
+}
+
+function loader(isLoading = false) {
+    const img = isLoading ? 'url(../icons/loading.gif)' : ''
+    $screen.style.backgroundImage = img
+}
 
 export async function findPokemon(id) {
     const pokemon = await getPokemon(id)
     const species = await getSpecies(id)
     const description = species.flavor_text_entries.find(entry => entry.language.name === 'es').flavor_text
+    const sprites = [pokemon.sprites.front_default]
 
-    $image.src = pokemon.sprites.front_default
-    $description.innerHTML = `
-            <h1>${pokemon.name}</h1>
-            <p>${description}</p>`
-    console.log(pokemon, species)
+    for (const spritesKey in pokemon.sprites){
+
+        if(spritesKey !== 'front_default' && pokemon.sprites[spritesKey] && spritesKey !== 'other' && spritesKey !== 'versions'){
+            sprites.push(pokemon.sprites[spritesKey])
+        }
+    }
+
+    console.log(sprites)
+
+    return {
+        id: pokemon.id,
+        name: pokemon.name,
+        sprites,
+        description
+    }
 }
 
+export async function setPokemon(id){
+    loader(true)
+    const pokemon = await findPokemon(id)
+    loader(false)
+    setImage(pokemon.sprites[0])
+    setDescription(pokemon.name, pokemon.description)
+
+    speech(`${pokemon.name}. ${pokemon.description}`)
+
+    return pokemon
+}
